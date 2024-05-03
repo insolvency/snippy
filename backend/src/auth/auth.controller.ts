fc647db525129b7c3c2b647c0d773ef8b897d3dd
insolvency <104585393+insolvency@users.noreply.github.com>
2024-05-03 08:44:00 +0000

    Login input validation

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { Public } from "./public.decorator";
+import { LoginDto } from "./dtos/login.dto";
 
 @Controller("auth")
 export class AuthController {
@@ -9,7 +10,7 @@ export class AuthController {
     @HttpCode(HttpStatus.OK)
     @Public()
     @Post("login")
-    login(@Body() loginDto: Record<string, any>) {
+    login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto.username, loginDto.password);
     }
 }
